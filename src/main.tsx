d095448dd6d3c30d0f6bef980344735a0c25fc55
Tristan Smith <ohsofast@live.com>
2024-12-07 13:35:16 -0500

    *-+ +
    +*-*

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import App from "./App.tsx";
 import "./index.css";
-import theme from "./styles/theme.tsx";
+
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={darkTheme}>
             <CssBaseline />
             <App />
         </ThemeProvider>
